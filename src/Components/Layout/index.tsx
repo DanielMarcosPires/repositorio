b0af2c41ebf913155cd677e29114a1ce286b0d6c
Daniel Marcos Pires <89848369+DanielMarcosPires@users.noreply.github.com>
2023-05-27 15:44:27 -0300

    Layout

--- src/Components/Layout/index.tsx
+++ src/Components/Layout/index.tsx
@@ -1,3 +1,4 @@
+import style from './scss/style.module.scss'
 import { ReactNode } from "react";
 
 type htmlChildren = {
@@ -5,8 +6,24 @@ type htmlChildren = {
 }
 export default function Layout({children}:htmlChildren) {
   return (
-    <div>
-        
-    </div>
+    <main className={style.LayoutGrid}>
+           <header className={style.LayoutGridCabecalho}>
+                <div className={style.LayoutGridCabecalhoMargem}>
+                    <h1>Logo</h1>
+                    <div>
+                        <p>switch</p>
+                        <p>notificação</p>
+                        <p>perfil</p>
+                    </div>
+                </div>
+           </header>
+           <aside className={style.LayoutGridSideBar}>
+            <h1>close</h1>
+                <nav>
+                    <a href="#">Home</a>
+                </nav>
+           </aside>
+           <h1>Repositorio</h1>
+    </main>
   )
 }
